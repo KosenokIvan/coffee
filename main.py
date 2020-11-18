b3d5b113f0215c21dbe4c7a9edcdab3052e01fe5
Ivan <kosenoki213@gmail.com>
2020-11-18 17:29:13 +0300

    QTableView заменён на QTableWidget. Вместо QSqlDatabase используется sqlite3

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
+import sqlite3
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QHeaderView
+from PyQt5.QtCore import Qt
 from PyQt5 import uic
 
 
 class CoffeeWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.cur = self.con.cursor()
         self.initUI()
         self.update_table()
 
@@ -14,13 +17,19 @@ class CoffeeWindow(QMainWindow):
         uic.loadUi("main.ui", self)
 
     def update_table(self):
-        db = QSqlDatabase.addDatabase("QSQLITE")
-        db.setDatabaseName("coffee.sqlite")
-        db.open()
-        model = QSqlTableModel(self, db)
-        model.setTable("coffee")
-        model.select()
-        self.tableView.setModel(model)
+        res = self.cur.execute("""SELECT * FROM coffee""").fetchall()
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setHorizontalHeaderLabels(["ID", "Название", "Степень обжарки", "Молотый",
+                                                    "Вкус", "Цена", "Объем упаковки"])
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                item = QTableWidgetItem(str(elem))
+                item.setFlags(Qt.ItemIsEnabled)
+                self.tableWidget.setItem(i, j, item)
+        self.tableWidget.resizeColumnsToContents()
 
 
 def excepthook(cls, value, traceback):
